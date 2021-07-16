78637631ddd4170c843ad217451cc7818ce8083c
okyraChan <okyra19chan@gmail.com>
2021-07-16 17:13:47 +0700

    product description

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,12 +1,40 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:shamo_tryit/theme.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
   const ProductPage({Key? key}) : super(key: key);
 
   @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  List images = [
+    'images/shoes_hiking_55.png',
+    'images/shoes_hiking_55.png',
+    'images/shoes_hiking_55.png'
+  ];
+
+  int currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
+    Widget indicator(int index) {
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: 2),
+        width: currentIndex == index ? 16 : 4,
+        height: 4,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: currentIndex == index ? primaryColor : Color(0xffC4C4C4),
+        ),
+      );
+    }
+
     Widget header() {
+      int index = -1;
+
       return Column(
         children: [
           Container(
@@ -31,7 +59,128 @@ class ProductPage extends StatelessWidget {
               ],
             ),
           ),
+          CarouselSlider(
+            items: images
+                .map(
+                  (e) => Image.asset(
+                    e,
+                    height: 310,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.cover,
+                  ),
+                )
+                .toList(),
+            options: CarouselOptions(
+                initialPage: 0,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                }),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: images.map((e) {
+              index++;
+              return indicator(index);
+            }).toList(),
+          ),
         ],
+      );
+    }
+
+    Widget content() {
+      return Container(
+        width: double.infinity,
+        margin: EdgeInsets.only(top: 17),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(24),
+            topRight: Radius.circular(24),
+          ),
+          color: bgColor1,
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(
+                  marginDefault, marginDefault, marginDefault, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'TERREX URBAN LOW',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 18,
+                          fontWeight: semiBold,
+                        ),
+                      ),
+                      Text(
+                        'Hiking',
+                        style: secondaryTextStyle.copyWith(fontSize: 12),
+                      ),
+                    ],
+                  ),
+                  Image.asset(
+                    'images/button_love.png',
+                    width: 46,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(
+                top: 20,
+                left: marginDefault,
+                right: marginDefault,
+              ),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: bgColor2,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Price starts from',
+                    style: primaryTextStyle,
+                  ),
+                  Text(
+                    '\$143,98',
+                    style: priceTextStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.all(marginDefault),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Description',
+                    style: primaryTextStyle.copyWith(fontWeight: medium),
+                  ),
+                  SizedBox(height: 12),
+                  Text(
+                    'Unpaved trails and mixed surfaces are easy when you have the traction and support you need. Casual enough for the daily commute.',
+                    style: subtittleTextStyle.copyWith(fontWeight: light),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       );
     }
 
@@ -40,6 +189,7 @@ class ProductPage extends StatelessWidget {
         body: ListView(
           children: [
             header(),
+            content(),
           ],
         ));
   }
